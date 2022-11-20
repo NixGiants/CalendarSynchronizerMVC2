40e640d66763e21262f97246875f21fb8dc79a0f
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 15:47:07 +0200

    Update Application DbContext

---
+++ CalendarSynchronizerWeb/DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public List<Schedule> Schedules { get; set; }
+    }
+}
