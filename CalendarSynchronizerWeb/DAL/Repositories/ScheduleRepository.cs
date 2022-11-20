80a097b30e581b0069e9ea50414862755d1ab208
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 15:47:15 +0200

    Create ScheduleRepository.cs

---
+++ CalendarSynchronizerWeb/DAL/Repositories/ScheduleRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    internal class ScheduleRepository : IScheduleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ScheduleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Schedule GetSchedule(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
