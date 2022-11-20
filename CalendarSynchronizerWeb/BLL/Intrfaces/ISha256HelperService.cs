3f89b4a3cdc818e477221c73132cbe1988bd4566
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 00:43:45 +0200

    Create ISha256HelperService.cs

---
+++ CalendarSynchronizerWeb/BLL/Intrfaces/ISha256HelperService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Intrfaces
+{
+    public interface ISha256HelperService
+    {
+        public string ComputeHash(string codeVerifier);
+    }
+}
