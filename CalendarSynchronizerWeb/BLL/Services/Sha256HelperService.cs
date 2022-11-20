372fec2b9d3cb71627f05b76760ced5150036a98
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 00:43:49 +0200

    Create Sha256HelperService.cs

---
+++ CalendarSynchronizerWeb/BLL/Services/Sha256HelperService.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Intrfaces;
+using IdentityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class Sha256HelperService : ISha256HelperService
+    {
+        public string ComputeHash(string codeVerifier)
+        {
+            using var sha256 = SHA256.Create();
+            var challengeBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(codeVerifier));
+            var codeChallenge = Base64Url.Encode(challengeBytes);
+            return codeChallenge;
+        }
+    }
+}
