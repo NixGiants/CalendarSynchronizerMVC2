e6e25326bc674b80b914ad83245e4164606ab01d
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 15:21:23 +0200

    Creating MVC Project

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalendarSynchronizerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
