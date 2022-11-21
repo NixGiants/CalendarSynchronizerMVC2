e7a3655b3729c6b45f227816ddaf011876e76277
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 10:42:10 +0200

    Add CodeSync Method

--- CalendarSynchronizerWeb/CalendarSynchronizerWeb/Controllers/GoogleAuthController.cs
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Controllers/GoogleAuthController.cs
@@ -1,6 +1,8 @@
 ﻿using BLL.Intrfaces;
+using BLL.Services;
 using CalendarSynchronizerWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Protocols;
 
 namespace CalendarSynchronizerWeb.Controllers
@@ -9,15 +11,17 @@ namespace CalendarSynchronizerWeb.Controllers
     {
         //private readonly IConfigurationManagerService<GoogleAuthCreds> configurationManager;
         private readonly ISha256HelperService sha256HelperService;
+        private readonly IGoogleOAuthService googleOAuthService;
 
-        public GoogleAuthController(ISha256HelperService sha256HelperService)
+        public GoogleAuthController(ISha256HelperService sha256HelperService, IGoogleOAuthService googleOAuthService)
         {
             this.sha256HelperService = sha256HelperService;
+            this.googleOAuthService = googleOAuthService;
         }
 
-        private const string RedirectUrl = "https://localhost:44329/GoogleOAuth/Code";
+        private const string RedirectUrl = "https://localhost:7060/GoogleAuth/Code";
         private const string CalendarScope = "https://www.googleapis.com/auth/calendar.readonly";
-        private const string PkceSessionKey = "codeVerifier";
+        private const string PkceSessionKey = "codeVerifier";   
 
         public IActionResult RedirectOnOAuthServer()
         {
@@ -27,11 +31,37 @@ namespace CalendarSynchronizerWeb.Controllers
 
             HttpContext.Session.SetString(PkceSessionKey, codeVerifier);
 
-            var url = GoogleOAuthService.GenerateOAuthRequestUrl(CalendarScope, RedirectUrl, codeChellange);
+            var url = googleOAuthService.GenerateOAuthRequestUrl(CalendarScope, RedirectUrl, codeChellange);
             return Redirect(url);
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> CodeAsync(string code)
+        {
+            try
+            {
+                var codeVerifier = HttpContext.Session.GetString(PkceSessionKey);
+
+                var tokenResult = await googleOAuthService.ExchangeCodeOnTokenAsync(code, codeVerifier, RedirectUrl);
+
+                //configurationManager.ChangeAppSettingValue(opt =>
+                //{
+                //    opt.RefreshToken = tokenResult.RefreshToken;
+                //    opt.AccessToken = tokenResult.AccessToken;
+                //});
+                return RedirectToAction("Privacy", "Home");
+
+               // return RedirectToAction("IncrementalSync", "GoogleCalendar");
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //TODO Error hadling logic
+            }
+        }
+
+
+            public IActionResult Index()
         {
             return View();
         }
