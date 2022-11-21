9c6d7dfd98c3be4ca6980376c1ce884bb28b71e9
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 10:42:30 +0200

    Create IGoogleOAuthService.cs

---
+++ CalendarSynchronizerWeb/BLL/Intrfaces/IGoogleOAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Intrfaces
+{
+    public interface IGoogleOAuthService
+    {
+        public string GenerateOAuthRequestUrl(string scope, string redirectUrl, string codeChallenge);
+        public Task<TokenRes> ExchangeCodeOnTokenAsync(string code, string codeVerifier, string redirectUrl);
+        public Task<TokenRes> RefreshTokenAsync(string refreshToken);
+    }
+}
