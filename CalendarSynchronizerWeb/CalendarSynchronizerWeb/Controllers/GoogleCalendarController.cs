cb0ee4b9d5bbc1f1d19194707282e4f8a463c6f0
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 14:51:48 +0200

    Create Controller

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Controllers/GoogleCalendarController.cs
@@ -0,0 +1,24 @@
+﻿using CalendarSynchronizerWeb.Managers.Interfaces;
+using Core.Models;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarSynchronizerWeb.Controllers
+{
+    public class GoogleCalendarController : Controller
+    {
+        private readonly IConfigurationManager<GoogleAuthCreds> _configurationManager;
+        private readonly ApplicationDbContext _context;
+        public GoogleCalendarController(IConfigurationManager<GoogleAuthCreds> configurationManager, ApplicationDbContext context)
+        {
+            _configurationManager = configurationManager;
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
