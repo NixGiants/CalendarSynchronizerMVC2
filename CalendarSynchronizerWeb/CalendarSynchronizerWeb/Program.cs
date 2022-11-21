78b78077e4a4fe8cea9f77a2a25f42122390699d
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 14:51:33 +0200

    Add Service

--- CalendarSynchronizerWeb/CalendarSynchronizerWeb/Program.cs
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Program.cs
@@ -1,6 +1,8 @@
 using BLL.Intrfaces;
 using BLL.Services;
+using CalendarSynchronizerWeb.Extensions;
 using CalendarSynchronizerWeb.Models;
+using Core.Models;
 using DAL;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,12 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//builder.Services.ConfigureWritable<GoogleAuthCreds>(builder.Configuration.GetSection("GoogleAuthCreds"));
+builder.Services.ConfigureWritable<GoogleAuthCreds>(builder.Configuration.GetSection("GoogleAuthCreds"));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddSession();
-//builder.Services.AddScoped<IConfigurationManagerService<GoogleAuthCreds>, ConfigurationManagerService<GoogleAuthCreds>>();
+
 builder.Services.AddScoped<ISha256HelperService, Sha256HelperService>();
 builder.Services.AddScoped<IGoogleOAuthService, GoogleOAuthService>();
+builder.Services.AddSession();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
