18d0b997da8f6dc1c95498fe6309b1410c3a2506
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 14:51:24 +0200

    Create ConfigurationManager.cs

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Managers/ConfigurationManager.cs
@@ -0,0 +1,57 @@
+﻿using BLL.Intrfaces;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using CalendarSynchronizerWeb.Managers.Interfaces;
+
+namespace CalendarSynchronizerWeb.Managers
+{
+    public class ConfigurationManager<T>: IConfigurationManager<T> where T : class, new()
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly IOptionsMonitor<T> _options;
+        private readonly IConfigurationRoot _configuration;
+        private readonly string _section;
+        private readonly string _file;
+        public ConfigurationManager(
+            IWebHostEnvironment environment,
+            IOptionsMonitor<T> options,
+            IConfigurationRoot configuration,
+            string section,
+            string file
+            )
+        {
+            _environment = environment;
+            _options = options;
+            _configuration = configuration;
+            _section = section;
+            _file = file;
+        }
+        public T Value => _options.CurrentValue;
+        public T Get(string name) => _options.Get(name);
+        public void ChangeAppSettingValue(Action<T> applyChanges)
+        {
+            var fileProvider = _environment.ContentRootFileProvider;
+            var fileInfo = fileProvider.GetFileInfo(_file);
+            var physicalPath = fileInfo.PhysicalPath;
+            var jObject = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(physicalPath));
+            var sectionObject = jObject.TryGetValue(_section, out JToken section) ?
+                JsonConvert.DeserializeObject<T>(section.ToString()) : (Value ?? new T());
+            applyChanges(sectionObject);
+            jObject[_section] = JObject.Parse(JsonConvert.SerializeObject(sectionObject));
+            File.WriteAllText(physicalPath, JsonConvert.SerializeObject(jObject, Formatting.Indented));
+            _configuration.Reload();
+        }
+
+        public T GetAppSettingValue()
+        {
+            var fileProvider = _environment.ContentRootFileProvider;
+            var fileInfo = fileProvider.GetFileInfo(_file);
+            var physicalPath = fileInfo.PhysicalPath;
+            var jObject = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(physicalPath));
+            var sectionObject = jObject.TryGetValue(_section, out JToken section) ?
+                JsonConvert.DeserializeObject<T>(section.ToString()) : (Value ?? new T());
+            return sectionObject;
+        }
+    }
+}
