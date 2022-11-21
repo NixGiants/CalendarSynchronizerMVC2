0db09a7509b1b07e8da1121e145e29ef017bc4b4
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-21 14:50:46 +0200

    Create GoogleAuthCreds.cs

---
+++ CalendarSynchronizerWeb/Core/Models/GoogleAuthCreds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class GoogleAuthCreds
+    {
+        public GoogleAuthCreds()
+        {
+            RefreshToken = "default";
+            AccessToken = "default";
+            SyncToken = "default";
+        }
+        public string RefreshToken { get; set; }
+        public string AccessToken { get; set; }
+        public string SyncToken { get; set; }
+    }
+}
