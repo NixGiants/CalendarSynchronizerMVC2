ba5e6944f1fa8d47585134207016b0794708ac5c
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-22 23:37:48 +0200

    Create ISendGridEmailService.cs

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Services/Interfaces/ISendGridEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace CalendarSynchronizerWeb.Services.Interfaces
+{
+    public interface ISendGridEmailService
+    {
+        public Task SendEmailAsync(string toEmail, string subject, string message);
+    }
+}
