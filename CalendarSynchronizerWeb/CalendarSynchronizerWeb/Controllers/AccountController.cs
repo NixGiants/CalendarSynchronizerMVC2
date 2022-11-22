d1e7d42f48b283301fd1230ad0b431c053c25d30
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-22 19:07:08 +0200

    Create Account Controller with some methods

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using CalendarSynchronizerWeb.ViewModels;
+using Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarSynchronizerWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(string? returnUrl = null)
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            registerViewModel.ReturnUrl = returnUrl;
+            return View(registerViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            returnUrl = returnUrl ?? Url.Content("~/");
+            loginViewModel.ReturnUrl = returnUrl;
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel, string? returnUrl = null)
+        {
+            registerViewModel.ReturnUrl = returnUrl;
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = registerViewModel.UserName,
+                    Email = registerViewModel.Email,
+
+                };
+                var result = await userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                ModelState.AddModelError("Password", "User couldn't be created password not good enough");
+            }
+            return View(registerViewModel);
+        }
+    }
+}
