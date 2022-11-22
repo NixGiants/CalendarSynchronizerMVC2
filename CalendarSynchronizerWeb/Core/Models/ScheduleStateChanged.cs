bf5f911a0b565446d31410023b4160109dae224c
Roman Riabenko <beatle.gaze@gmail.com>
2022-11-22 16:30:56 +0200

    Add SheduleStateChanged

---
+++ CalendarSynchronizerWeb/Core/Models/ScheduleStateChanged.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Core.Models
+{
+    public enum State
+    {
+        Create = 1,
+        Update,
+        Delete
+    }
+    public class ScheduleStateChanged
+    {
+        public Schedule Source { get; set; }
+        public Schedule Target { get; set; }
+        public State Action { get; set; }
+    }
+}
