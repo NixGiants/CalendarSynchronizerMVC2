2157aa58775b46c2d5a6013d04b5bf70455d0101
Roman Riabenko <beatle.gaze@gmail.com>
2022-11-22 17:37:56 +0200

    Create GoogleCalendarService.cs

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Services/GoogleCalendarService.cs
@@ -0,0 +1,68 @@
+﻿using CalendarSynchronizerWeb.Managers.Interfaces;
+using Core.Models;
+using Google;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+
+
+namespace BLL.Services
+{
+    public class GoogleCalendarService
+    {
+        private readonly IConfigurationManager<GoogleAuthCreds> _configurationManager;
+        private CalendarService _service;
+        public GoogleCalendarService(IConfigurationManager<GoogleAuthCreds> configurationManager)
+        {
+            _configurationManager = configurationManager;
+            _service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = new CustomUserCredential(_configurationManager.Value.AccessToken),
+                ApplicationName = "AppName"
+            });
+
+        }
+        public async Task<CalendarList> GetAllCalendarListAsync()
+        {
+
+            try
+            {
+                return await _service.CalendarList.List().ExecuteAsync();
+            }
+            catch (GoogleApiException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //Some Exception handling logic
+                throw ex;
+
+            }
+
+        }
+
+        public async Task<Events> GetCalendarEventsList(string calendarId = "primary")
+        {
+            try
+            {
+                var request = _service.Events.List(calendarId);
+                var res = await request.ExecuteAsync();
+                return res;
+            }
+            catch (GoogleApiException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //Some Exception handling logic
+                throw ex;
+            }
+        }
+        public CalendarService GetCalenderService()
+        {
+            return _service;
+        }
+    }
+}
