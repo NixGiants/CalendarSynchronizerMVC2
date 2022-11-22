bc6195f1b3b6e331a47edd8f51c376e238ecf0dd
Roman Riabenko <beatle.gaze@gmail.com>
2022-11-22 16:49:50 +0200

    Add IRepository

---
+++ CalendarSynchronizerWeb/Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using System.Linq.Expressions;
+
+namespace Core.Interfaces
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task CreateAsync(T entity);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetAsync(Guid id);
+        Task RemoveAsync(Guid id);
+        Task RemoveAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task HandleScheduleStatesAsync(IEnumerable<ScheduleStateChanged> scheduleChanges);
+        Task<bool> Any();
+    }
+}
