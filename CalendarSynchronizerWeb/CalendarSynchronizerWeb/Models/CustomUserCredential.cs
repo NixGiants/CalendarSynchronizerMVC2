e865ef4f4d9a95589279ec763eb994867780ea1a
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-22 17:53:47 +0200

    Create CustomUserCredential.cs

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/Models/CustomUserCredential.cs
@@ -0,0 +1,26 @@
+﻿using Google.Apis.Http;
+using System.Net.Http.Headers;
+
+namespace CalendarSynchronizerWeb.Models
+{
+    
+    public class CustomUserCredential : IHttpExecuteInterceptor, IConfigurableHttpClientInitializer
+    {
+        private readonly string accessToken;
+
+        public CustomUserCredential(string accessToken)
+        {
+            this.accessToken = accessToken;
+        }
+
+        public void Initialize(ConfigurableHttpClient httpClient)
+        {
+            httpClient.MessageHandler.ExecuteInterceptors.Add(this);
+        }
+
+        public async Task InterceptAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+    }
+}
