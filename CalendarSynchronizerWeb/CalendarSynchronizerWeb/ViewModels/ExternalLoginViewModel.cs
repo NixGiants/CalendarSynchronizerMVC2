971f8ce5685dc6afb2907b332bc49b65f2f36037
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-24 14:16:08 +0200

    Create ExternalLoginViewModel.cs

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/ViewModels/ExternalLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalendarSynchronizerWeb.ViewModels
+{
+    public class ExternalLoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
