558ac8fdf41dbe4c3447cff265830a2317dbbc1a
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-25 13:21:22 +0200

    Create View Models for Schedule

---
+++ CalendarSynchronizerWeb/CalendarSynchronizerWeb/ViewModels/Schedule/ScheduleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalendarSynchronizerWeb.ViewModels.Schedule
+{
+    public class ScheduleViewModel
+    {
+        public Guid? Id { get; set; }
+        public string? Subject { get; set; }
+        [Required]
+        //[DataType(DataType.DateTime)]
+        public DateTime? StartTime { get; set; }
+        public string? StartTimeZone { get; set; }
+        [Required]
+        //[DataType(DataType.DateTime)]
+        public DateTime? EndTime { get; set; }
+        public string? EndTimeZone { get; set; }
+        public string? Status { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public bool IsAllDay { get; set; } = false;
+        public string? CalendarId { get; set; }
+
+    }
+}
