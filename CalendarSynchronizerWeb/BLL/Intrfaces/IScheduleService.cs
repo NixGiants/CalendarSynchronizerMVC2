c268a4c7e79a54d137c680403bd2d0cda4497023
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-25 13:18:27 +0200

    Create Schedule ServiceAnd Interface

---
+++ CalendarSynchronizerWeb/BLL/Intrfaces/IScheduleService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Intrfaces
+{
+    public interface IScheduleService
+    {
+        public Task Create(Schedule schedule);
+        public Task<Schedule?> GetSchedule(Guid id);
+        public Task<List<Schedule>> GetCalendarSchedules(string calendarId);
+
+        public Task Delete(Guid id);
+
+        public Task Update(Guid id, Schedule schedule);
+    }
+}
---
+++ CalendarSynchronizerWeb/BLL/Services/ScheduleService.cs
@@ -0,0 +1,103 @@
+﻿using BLL.Intrfaces;
+using Core.Models;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ScheduleService : IScheduleService
+    {
+        private readonly IScheduleRepository scheduleRepository;
+
+        private readonly ILogger<ScheduleService> logger;
+
+        public ScheduleService(ILogger<ScheduleService> logger, IScheduleRepository scheduleRepository)
+        {
+            this.logger = logger;
+            this.scheduleRepository = scheduleRepository;
+        }
+
+        public async Task Create(Schedule schedule)
+        {
+            try
+            {
+                await scheduleRepository.Create(schedule);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            try
+            {
+                await scheduleRepository.Delete(id);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return;
+        }
+
+        public async Task<List<Schedule>> GetCalendarSchedules(string calendarId)
+        {
+            if (String.IsNullOrEmpty(calendarId))
+            {
+                throw new ArgumentException("Bad Id was given");
+            }
+
+            List<Schedule> schedules = new List<Schedule>();
+
+            try
+            {
+                schedules = await scheduleRepository.GetCalendarSchedules(calendarId);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return schedules;
+        }
+
+        public async Task<Schedule?> GetSchedule(Guid id)
+        {
+            try
+            {
+                var schedule = await scheduleRepository.GetSchedule(id);
+                return schedule;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return null;
+        }
+
+        public async Task Update(Guid id, Schedule schedule)
+        {
+            try
+            {
+                await scheduleRepository.Update(id, schedule);
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return;
+        }
+    }
+}
