669b536b113c67a0ea96b47ed20048188fa8c2cf
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-28 07:45:09 +0200

    add interface for google calendar service

---
+++ CalendarSynchronizerWeb/BLL/Intrfaces/IGoogleCalendarService.cs
@@ -0,0 +1,15 @@
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+
+
+namespace BLL.Intrfaces
+{
+    public interface IGoogleCalendarService
+    {
+        public Task<CalendarList> GetAllCalendarListAsync();
+
+        public Task<Events> GetCalendarEventsList(string calendarId = "primary");
+        public CalendarService GetCalenderService();
+
+    }
+}
