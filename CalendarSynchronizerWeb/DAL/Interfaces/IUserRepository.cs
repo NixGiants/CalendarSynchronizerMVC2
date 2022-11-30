4f1291d95c77f2d11dded2c037c7e356e9fcd576
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-30 15:46:00 +0200

    Create IUserRepository.cs

---
+++ CalendarSynchronizerWeb/DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<List<AppUser>> GetAll();
+        public Task<AppUser> GetById(string userId);
+        public Task<UserClaimsViewModel> GetForClaims(string userId);
+        public Task ManageClaims(UserClaimsViewModel viewModel);
+        public Task Update(AppUser user, string userId);
+        public Task Delete(string userId);
+    }
+}
