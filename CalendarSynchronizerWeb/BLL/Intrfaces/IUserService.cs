ca47ba5625df10da20fca0f90b2bb8fb3bad5079
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-30 15:46:13 +0200

    Create IUserService.cs

---
+++ CalendarSynchronizerWeb/BLL/Intrfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Intrfaces
+{
+    public interface IUserService
+    {
+        public Task<List<AppUser>> GetAll(string searchString);
+        public Task<AppUser?> GetById(string userId);
+        public Task Update(AppUser user, string userId);
+        public Task Delete(string userId);
+        public Task<UserClaimsViewModel?> GetForClaims(string userId);
+        public Task ManageClaims(UserClaimsViewModel viewModel);
+    }
+}
